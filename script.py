c7a03225ad9232ceb93e1504e74bd3a76c226434
GuillaumeMichel02 <73992969+GuillaumeMichel02@users.noreply.github.com>
2022-04-26 18:59:14 +0200

    add some missing mons, save and load, number of known mons, jump to gens

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from select import select
 import numpy
+import json
 import tkinter as tk
 from tkinter import ttk
 from unidecode import unidecode
@@ -9,20 +10,54 @@ result = False
 first = True
 
 known = {}
+gen = {1:1, 2:152, 3:252, 4:387, 5:495, 6:650, 7:722, 8:810, 9:899}
+current_gen = 1
+number_known = 0
 
 with open('liste pokemon.txt', 'r', encoding='utf-8') as fd:
     pokemon_list = fd.read().split(' ')
     pokemon_list.insert(0,"default")
 
 for i in range(1,899):
-    known[i] = 0
+    known[str(i)] = 0
 
-known[0] = 6
+known[str(0)] = 3
+
+def save():
+    global known
+    global current_gen
+    with open('save.txt', 'w') as save_file:
+        save_file.write(json.dumps(known))
+
+def load():   
+    global known
+    global number_known
+    global current_gen
+    with open('save.txt', 'r') as save_file:
+        known = json.load(save_file)
+    
+    number_known = 0
+    for i in range(1,899):
+        if known[str(i)]>2:
+            number_known = number_known + 1
+
+    if number_known >= 898:
+        return
+
+    while sum(known[str(i)] for i in range(gen[current_gen],gen[current_gen+1])) >= (gen[current_gen+1]-gen[current_gen])*3:
+        current_gen = current_gen + 1
+        if current_gen == 9:
+            current_gen = 1  
+    
+
 
 def generate_pkm():
+    global number_known
     nbr = 0
-    while known[nbr]>2:
-        nbr = numpy.random.randint(1,899)
+    if number_known >= 898:
+        return 1, pokemon_list[1]
+    while known[str(nbr)]>2 or nbr > 898:
+         nbr = numpy.random.randint(gen[current_gen],gen[current_gen+1])
     return nbr, pokemon_list[nbr]
 
 def generate_shiny():
@@ -41,8 +76,19 @@ def enter_pressed(image_label, prompt):
     global select_poke
     global name_poke
     global result
+    global current_gen
+    global number_known
+    if number_known >= 898:
+        prompt.config(text = "Tu les connais tous !")
     if first:
         return
+    if entry.get().isnumeric():
+        generation = int(entry.get(), base=10)
+        if generation > 0 and generation < 9:
+            current_gen = generation
+            result = True
+            known[str(select_poke)] = known[str(select_poke)] - 1
+        
     if not result:
         print(unidecode(entry.get()).lower())
         print(unidecode(name_poke).lower())
@@ -51,23 +97,32 @@ def enter_pressed(image_label, prompt):
             prompt.config(text = name_poke)
             result = True
             return
+
     if entry.get() == 's':
-        known[select_poke] = 3
-    known[select_poke] = known[select_poke]+1
+        known[str(select_poke)] = 2
+    known[str(select_poke)] = known[str(select_poke)]+1
+    if known[str(select_poke)]>2:
+        number_known = number_known+1
+
+    while sum(known[str(i)] for i in range(gen[current_gen],gen[current_gen+1])) >= (gen[current_gen+1]-gen[current_gen])*3:
+        current_gen = current_gen + 1
+        if current_gen == 9:
+            current_gen = 1  
+    
     entry.delete(0, 'end')
     select_poke, name_poke = generate_pkm()
     shiny = generate_shiny()
     new_pkm_image = get_image(select_poke, shiny)
     image_label.config(image = new_pkm_image)
     image_label.image = new_pkm_image
-    prompt.config(text = "Quel est ce pokémon ?")
+    prompt.config(text = "Quel est ce pokémon ? Nombre appris : "+str(number_known).zfill(3))
     result = False
     return
 
 
 root = tk.Tk()
 root.title('Pokemon Guesser')
-root.geometry('640x720')
+root.geometry('1000x720')
 
 
 
@@ -76,7 +131,7 @@ pkm_image = get_image(select_poke, shiny)
 
 image_label = tk.Label(root, image = pkm_image)
 
-prompt = tk.Label(root, text="Quel est ce pokémon ?", font = "Arial 24 bold")
+prompt = tk.Label(root, text="Quel est ce pokémon ? Nombre appris : "+str(number_known).zfill(3), font = "Arial 24 bold")
 
 entry = tk.Entry(font = "Arial 24", width = 10, justify='center')
 
@@ -84,11 +139,20 @@ entry = tk.Entry(font = "Arial 24", width = 10, justify='center')
 root.bind('<Return>', lambda event: enter_pressed(image_label, prompt))
 first = False
 
+savebtn = tk.Button(root, text='Save', command=save)
+loadbtn = tk.Button(root, text='Load', command=load)
+
 image_label.pack()
 
 prompt.pack()
 
 entry.pack()
+
+savebtn.pack()
+loadbtn.pack()
+
 entry.focus()
+
+
 
 root.mainloop()
